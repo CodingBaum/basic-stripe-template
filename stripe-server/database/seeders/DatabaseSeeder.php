d74fd7b8ceaee457007e8ee02f497ff10f22244e
Daniel <9132@htl.rennweg.at>
2024-02-28 09:21:55 +0100

    recreated seeding

--- stripe-server/database/seeders/DatabaseSeeder.php
+++ stripe-server/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('products')->delete();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $products = Http::withHeaders([
+            'Authorization' => 'Bearer ' . env("STRIPE_SECRET_KEY")
+        ])->get('https://api.stripe.com/v1/products')["data"];
+
+        foreach ($products as $product){
+            $price = Http::withHeaders([
+                'Authorization' => 'Bearer ' . env("STRIPE_SECRET_KEY")
+            ])->get('https://api.stripe.com/v1/prices/' . $product["default_price"]);
+
+            DB::table('products')->insert([
+                'prod_id' => $product["id"],
+                'name' => $product["name"],
+                'description' => $product["description"],
+                'image' => $product["name"],
+                'price' => $price["unit_amount"]/100,
+            ]);
+        }
     }
 }
