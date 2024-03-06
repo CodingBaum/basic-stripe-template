bd0fd1847dd6f69850e6b301eec2e534f50ff782
Daniel <9132@htl.rennweg.at>
2024-03-06 08:41:29 +0100

    changed price range

--- stripe-server/database/migrations/2024_02_19_074338_create_orders_table.php
+++ stripe-server/database/migrations/2024_02_19_074338_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string("status");
-            $table->decimal("total_price", 6, 2);
+            $table->decimal("total_price", 8, 2);
             $table->string("session_id");
             $table->timestamps();
         });
