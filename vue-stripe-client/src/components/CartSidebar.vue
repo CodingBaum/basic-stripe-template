c5a3fdc85d7bf23046eeca7572eb3e76012e9008
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2024-03-20 08:39:04 +0100

    fonts and start of sidebar

--- vue-stripe-client/src/components/CartSidebar.vue
+++ vue-stripe-client/src/components/CartSidebar.vue
@@ -17,23 +17,85 @@ function checkout() {
 </script>
 
 <template>
-    <div class="car-sidebar-wrapper">
-        <div v-for="(product, index) in cart" :key="index">
-            {{ product.name }}
-            {{ product.quantity }}
-            <button @click="addToCart(product)">+</button>
-            <button @click="removeFromCart(product)">-</button>
+    <div class="cart-sidebar-wrapper">
+        <div class="cart-sidebar-container">
+            <nav class="close-nav">
+                <button class="close-button">CLOSE</button>
+            </nav>
+            <div class="product-container">
+                <div class="cart-item" v-for="(product, index) in cart" :key="index">
+                    <div class="hr"></div>
+                    <div class="product-details">
+                        <div class="product-name-quantity">
+                            <img src="https://placehold.co/75x75">
+                            {{ product.name }}
+                            {{ product.quantity }}
+                            <button @click="addToCart(product)">+</button>
+                            <button @click="removeFromCart(product)">-</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Subtotal</p>
+                <p>€2600</p>
+            </div>
+            <button v-if="cart.length" @click="checkout">Checkout</button>
         </div>
-        <button v-if="cart.length" @click="checkout">Checkout</button>
     </div>
 </template>
 
 <style scoped>
-.car-sidebar-wrapper {
+.cart-sidebar-wrapper {
     position: fixed;
     height: 100vh;
-    width: 100px;
-    background: rebeccapurple;
+    width: 100%;
     right: 0;
+    background: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: flex-end;
+}
+
+.cart-sidebar-container {
+    width: 350px;
+    background: white;
+    padding: 10px 25px;
+}
+
+.close-nav {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 20px;
+}
+
+.close-nav .close-button {
+    background: none;
+    border: none;
+}
+
+.product-container {
+
+}
+
+.cart-item {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-bottom: 10px;
+}
+
+.cart-item .hr {
+    height: 1px;
+    background: #e2e8f0;
+}
+
+.cart-item .product-details {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+}
+
+.product-name-quantity {
+
 }
 </style>
