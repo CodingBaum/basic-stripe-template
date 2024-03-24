40dfab80f8dc12a04df66fcec69455de6cdde499
Daniel <9132@htl.rennweg.at>
2024-03-24 19:19:19 +0100

    detail popup done

--- vue-stripe-client/src/components/Product.vue
+++ vue-stripe-client/src/components/Product.vue
@@ -14,7 +14,7 @@ const popupOpen = ref(false);
   <div class="product-container">
     <img :src="product.image" @click="popupOpen=!popupOpen"/>
     <div class="info-section">
-      <div class="title" :title="product.name"  @click="popupOpen=!popupOpen">{{ product.name }}</div>
+      <div class="title" :title="product.name" @click="popupOpen=!popupOpen">{{ product.name }}</div>
       <div class="price">{{ product.price }} {{ product.currency_code == "eur" ? "€" : "$" }}</div>
       <div class="button-container">
         <button @click="addToCart(product)">Add to cart</button>
@@ -22,10 +22,29 @@ const popupOpen = ref(false);
       </div>
     </div>
   </div>
-  <div :class="{'product-popup-wrapper':true, 'popup-open':popupOpen, 'popup-closed':!popupOpen}"
-       @click="popupOpen=false">
+  <div :class="{'product-popup-wrapper':true, 'popup-open':popupOpen, 'popup-closed':!popupOpen}">
     <div class="product-popup">
-      HALLO
+      <nav class="close-nav">
+        <div class="close-button">
+          <button @click="popupOpen = false">CLOSE</button>
+          <div class="button-line"/>
+        </div>
+      </nav>
+      <div class="content-wrapper">
+        <img :src="product.image" alt="product image" class="detail-image">
+        <div class="info-wrapper">
+          <div :title="product.name" class="title">{{ product.name }}</div>
+          <div class="description">{{ product.description }}</div>
+          <div class="action-container">
+            <div class="price"><span class="price-label">Price per Unit: </span>{{ product.price }} {{ product.currency_code == "eur" ? "€" : "$" }}
+            </div>
+            <div class="button-container">
+              <button @click="()=>{addToCart(product);popupOpen = false}">Add to cart</button>
+              <div class="button-line"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -114,9 +133,11 @@ button:hover {
   cursor: pointer;
 }
 
-.title:hover, img:hover{
+.title:hover, img:hover {
   cursor: pointer;
 }
+
+/* Detail-PopUp */
 
 .product-popup-wrapper {
   position: fixed;
@@ -147,11 +168,98 @@ button:hover {
   animation-name: fade-in;
 }
 
-.product-popup{
-  height: 500px;
-  width: clamp(200px,80vw,800px);
-  background-color: red;
-  z-index: 3;
+.product-popup {
+  max-height: 500px;
+  width: clamp(200px, 80vw, 800px);
+  background-color: white;
+  border-radius: 5px;
+  padding: 25px;
+}
+
+.close-nav {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+}
+
+.close-button > button {
+  background: none;
+  border: none;
+}
+
+.button-line {
+  height: 1px;
+  width: 0;
+  background-color: black;
+}
+
+.close-button:hover {
+  cursor: pointer;
+
+  * {
+    cursor: pointer;
+  }
+
+  .button-line {
+    width: 100%;
+    animation-name: underline;
+    animation-duration: 0.25s;
+    animation-timing-function: ease-out;
+  }
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+
+}
+
+.detail-image {
+  margin: auto 0 auto 0;
+}
+
+.info-wrapper {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 20px;
+
+  max-height: 400px;
+
+  .title {
+    width: fit-content;
+
+    &:hover {
+      cursor: initial;
+    }
+  }
+
+  .description {
+    overflow-y: auto;
+  }
+}
+
+.action-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+@media screen and (max-width: 650px) {
+  .content-wrapper {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .product-popup{
+    max-height: fit-content;
+
+    .price-label{
+      display: none;
+    }
+  }
 }
 
 @keyframes fade-in {
